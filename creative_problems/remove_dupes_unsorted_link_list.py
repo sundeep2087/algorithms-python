538f5fbb1041c022b831dd4a7f32d36bb31c99a6
Sai Sundeep Rayidi <saisundeep@vt.edu>
2024-07-02 02:54:47 -0400

    Update: Singly Linked List

--- creative_problems/remove_dupes_unsorted_link_list.py
+++ creative_problems/remove_dupes_unsorted_link_list.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-from intermediate_data_structures.oneway_linked_list_implementation import LinkedList
+from intermediate_data_structures.singly_linked_list import LinkedList
 
 
 def remove_duplicates(unsorted_list):
