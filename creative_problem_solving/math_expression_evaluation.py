3ba1e807e5d39e6288c457f6143ed551ff8d241d
Sai Sundeep Rayidi <saisundeep@vt.edu>
2024-07-25 09:28:24 -0400

    update math expr

--- creative_problem_solving/math_expression_evaluation.py
+++ creative_problem_solving/math_expression_evaluation.py
@@ -5,8 +5,7 @@ def run_test_client():
     expressions_list = [
         "(2+3)*5+11*(8-9)"
     ]
-    evaluate_expression()
 
 
 if __name__ == "__main__":
-    run_testclient()
+    run_test_client()
