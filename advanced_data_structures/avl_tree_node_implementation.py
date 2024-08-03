1426444e60f5d7464134ba5c203b7cfdd9c6594a
Sai Sundeep Rayidi <saisundeep@vt.edu>
2024-08-03 03:17:22 -0400

    Add AVL Tree

--- advanced_data_structures/avl_tree_node_implementation.py
+++ advanced_data_structures/avl_tree_node_implementation.py
@@ -36,6 +36,8 @@ Contact:
 
 """
 
+from graphviz import Digraph
+
 
 class AVLTreeNode:
     def __init__(self, key, left=None, right=None):
@@ -43,6 +45,7 @@ class AVLTreeNode:
         self.left = left
         self.right = right
         self.height = 1
+        self.count = 1
 
 
 class AVLTree:
@@ -53,15 +56,15 @@ class AVLTree:
     def is_empty(self):
         return self.root is None
 
-    @staticmethod
-    def get_height(self, node=None):
+    def get_height(self, node):
         return node.height if node else 0
 
     def get_balance(self, node):
         return self.get_height(node.left) - self.get_height(node.right) if node else 0
 
     def update_height(self, node):
-        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+        if node:
+            node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
 
     def rotate_left(self, node):
         y = node.right
@@ -96,6 +99,7 @@ class AVLTree:
         elif key > root.key:
             root.right = self._insert_key(root.right, key)
         else:
+            root.count += 1
             return root
 
         self.update_height(root)
@@ -111,7 +115,7 @@ class AVLTree:
 
         # Right-Left Case
         if balance < -1 and key < root.right.key:
-            root.right = self.rotate_left(root.right)
+            root.right = self.rotate_right(root.right)
             return self.rotate_left(root)
 
         # Left-Right Case
@@ -121,9 +125,66 @@ class AVLTree:
 
         return root
 
-
-
-
     def insert(self, key):
         self.root = self._insert_key(self.root, key)
 
+    # Traversal Methods
+    def _recursive_inorder_traversal(self, node, result):
+        if not node:
+            return
+        self._recursive_inorder_traversal(node.left, result)
+        result.extend([node.key] * node.count)
+        self._recursive_inorder_traversal(node.right, result)
+
+    def inorder_traversal(self, node=None):
+        if not self.root:
+            return
+        result = []
+        if not node:
+            self._recursive_inorder_traversal(self.root, result)
+        self._recursive_inorder_traversal(node, result)
+        return result
+
+    # Tree Visualization Methods
+    def _visualize(self, node, graph):
+        if node is not None:
+            if node.left:
+                graph.node(str(node.key))
+                graph.edge(str(node.key), str(node.left.key))
+                self._visualize(node.left, graph)
+            if node.right:
+                graph.node(str(node.key))
+                graph.edge(str(node.key), str(node.right.key))
+                self._visualize(node.right, graph)
+
+    def visualize(self):
+        dg = Digraph()
+        if self.root:
+            dg.node(str(self.root.key))
+            self._visualize(self.root, dg)
+        dg.render("./data/avl_tree", format="png", cleanup=False)
+
+
+def run_test_client():
+    avl_tree = AVLTree()
+    avl_tree.insert(40)
+    avl_tree.insert(27)
+    avl_tree.insert(1)
+    avl_tree.insert(35)
+    avl_tree.insert(41)
+    avl_tree.insert(8)
+    avl_tree.insert(32)
+    avl_tree.insert(36)
+    avl_tree.insert(13)
+    avl_tree.insert(44)
+    avl_tree.insert(8)
+    avl_tree.insert(8)
+    avl_tree.insert(8)
+
+    inorder_traversal = avl_tree.inorder_traversal()
+    print(inorder_traversal)
+    avl_tree.visualize()
+
+
+if __name__ == "__main__":
+    run_test_client()
